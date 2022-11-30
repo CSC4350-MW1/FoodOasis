bc780f66fc42fcd1e7319a7bdb0e707fe11de32f
josiahdas <107151821+josiahdas@users.noreply.github.com>
2022-11-30 12:45:15 -0500

    Add files via upload

---
+++ loginsystem/searchpage2.php
@@ -0,0 +1,14 @@
+<?php
+$searchres = $_POST['search'];
+
+if ($searchres == '30097') {
+
+    include 'myarea.html';
+
+} else if ($searchres == '30303') {
+ 
+    include 'gsuarea.html';
+
+}
+
+?>
